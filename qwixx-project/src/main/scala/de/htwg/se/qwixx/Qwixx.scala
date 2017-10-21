081de9b76e6a19aa676db7569768af1cb6c2eb5d
jennykottmann <jenny.kottmann@arcor.de>
2017-10-21 20:00:51 +0200

    initial commit

---
+++ qwixx-project/src/main/scala/de/htwg/se/qwixx/Qwixx.scala
@@ -0,0 +1,11 @@
+package de.htwg.se.qwixx
+
+import de.htwg.se.qwixx.model.Player
+import de.htwg.se.qwixx.model.Player
+
+object Qwixx {
+  def main(args: Array[String]): Unit = {
+    val student = Player("Your Name")
+    println("Hello, " + student.name)
+  }
+}
