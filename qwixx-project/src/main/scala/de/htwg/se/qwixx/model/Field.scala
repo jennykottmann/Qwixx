68a4063c370446be326d5bd9b09796655816ba83
jennykottmann <jenny.kottmann@arcor.de>
2017-11-03 09:20:15 +0100

    added classes Row and Field

---
+++ qwixx-project/src/main/scala/de/htwg/se/qwixx/model/Field.scala
@@ -0,0 +1,3 @@
+package de.htwg.se.qwixx.model
+
+case class Field(player:String, redRow:Row, yellowRow:Row, greenRow:Row, blueRow:Row, fail:Int)
---
+++ qwixx-project/src/main/scala/de/htwg/se/qwixx/model/Row.scala
@@ -0,0 +1,5 @@
+package de.htwg.se.qwixx.model
+
+class Row {
+  
+}
