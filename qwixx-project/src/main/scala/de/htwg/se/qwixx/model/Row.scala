46e4b9a74e9838800410c811c4941a7b68af687a
jennykottmann <jenny.kottmann@arcor.de>
2017-11-09 00:21:32 +0100

    added List to Row

--- qwixx-project/src/main/scala/de/htwg/se/qwixx/model/Row.scala
+++ qwixx-project/src/main/scala/de/htwg/se/qwixx/model/Row.scala
@@ -1,3 +1,16 @@
 package de.htwg.se.qwixx.model
 
-case class Row(cells:Array[Cell], closedFlag:Boolean, colour: String)
+case class Row(crossedRow:List[Int], closedFlag:Boolean, colour: String) {
+  // Row besteht aus einer Liste (Int), einem Flag für closed und einer Farbe
+  var crossed = List[Int]
+
+  // Methode prüft, ob gewürfelte Zahl > bzw < des heads der Liste ist
+  def crossPossible: Unit = {
+  }
+
+
+  // Methode zum ankreuzen (an Liste wird Zahl angehängt
+  def cross {
+    // neue Zahl soll vorne angehängt werden
+  }
+}
